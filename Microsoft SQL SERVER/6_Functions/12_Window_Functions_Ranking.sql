d08aa0b6bbba7acb56c9a76db9c18f7ff7021629
ShubhamShr123 <shubhamshr@zohomail.in>
2026-01-04 11:41:52 +0530

    learnt the usecases of row_number() function

--- Microsoft SQL SERVER/6_Functions/12_Window_Functions_Ranking.sql
+++ Microsoft SQL SERVER/6_Functions/12_Window_Functions_Ranking.sql
@@ -42,3 +42,98 @@ SELECT
     Sales,
     DENSE_RANK() OVER(ORDER BY Sales DESC) as SalesRank
 from Sales.Orders;
+
+/*(row_number() = unique rank without gaps and doesnt consider ties,
+rank() = shared rank with gaps & handles ties,
+dense_rank() = shared rank without gaps & handles ties*/
+
+
+--ROW_NUMBER()
+--use cases - TOP N ANALYSIS
+--find the top highest sales for each product
+SELECT
+    OrderID,
+    OrderDate,
+    ProductID,
+    Sales,
+    ROW_NUMBER() OVER(PARTITION BY ProductID ORDER BY Sales desc) as Rank_by_Prod 
+from Sales.Orders;
+--now to filter above data we have to use subqueries
+SELECT *
+FROM(
+    SELECT
+        OrderID,
+        OrderDate,
+        ProductID,
+        Sales,
+        ROW_NUMBER() OVER(PARTITION BY ProductID ORDER BY Sales desc) as Rank_by_Prod 
+    from Sales.Orders)t
+WHERE Rank_by_Prod = 1;
+
+--BOTTOM N ANALYSIS
+--find the lowest 2 customers based on their total sales
+--with more details such as fullname, country name
+--to filter we are using the subquery
+SELECT
+    t.CustomerID,
+    CONCAT(c.FirstName,' ' ,c.LastName, '-', c.Country) as FullNameWithCountry,
+    t.Total_Sales,
+    t.Rank_by_cust
+FROM(
+SELECT 
+    CustomerID,
+    SUM(Sales) as Total_Sales,
+    ROW_NUMBER() OVER(ORDER BY SUM(Sales) ASC) as Rank_by_cust
+from Sales.Orders
+GROUP BY CustomerID)t
+INNER JOIN Sales.Customers as c
+on t.CustomerID = c.CustomerID
+WHERE t.Rank_by_cust <= 2;
+
+--GENERATE UNIQUE IDs
+--these unique id's can be used as an identifier
+-- and used for paginating (process of breaking down a large data set into smaller manageable chunks)
+--assign unique IDs to the rows of the ordersarchieve table
+SELECT
+    OrderID,
+    OrderDate,
+    CustomerID,
+    Sales,
+    ROW_NUMBER() OVER(ORDER BY OrderID,OrderDate) as Unique_ID
+from Sales.OrdersArchive;
+
+--IDENTIFY DUPLICATES
+--identify duplicates rows in the table ordersarchive and return a clean result without duplicates
+/*
+PARTITION BY OrderID - Divides the data into groups by OrderID. The row numbering resets to 1 for each new OrderID group
+ORDER BY CreationTime - Within each OrderID partition, rows are numbered in the order of CreationTime (earliest first)
+*/
+SELECT *
+FROM (
+SELECT
+    OrderID,
+    ProductID,
+    OrderDate,
+    CustomerID,
+    ROW_NUMBER() OVER(PARTITION BY OrderID ORDER BY CreationTime) as rn
+from Sales.OrdersArchive)t
+where rn=1;
+--to see any duplicate data
+SELECT *
+FROM (
+SELECT
+    OrderID,
+    ProductID,
+    OrderDate,
+    CustomerID,
+    ROW_NUMBER() OVER(PARTITION BY OrderID ORDER BY CreationTime) as rn
+from Sales.OrdersArchive)t
+where rn>1;
+
+--SUMMARY (ROW_NUMBER()) USECASES
+/*
+TOP N ANALYSIS
+BOTTOM N ANALYSIS
+ASSIGN UNIQUE IDS for duplicacy check
+QUALITY CHECKS: Identify Duplicates
+*/
